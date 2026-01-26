da7c60d42ce100928c1bb30f67cad3119e2ad0d7
Vibhinn Gautam <bhagwan.vibhinn@hotmail.com>
2026-01-26 09:35:39 -0500

    minor bug fix

--- src/components/returnbutton.jsx
+++ src/components/returnbutton.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 function ReturnButton(props){
     let navigate = useNavigate();
     return(<>
-    <div className={"h-16 p-4 w-1/2 lg: w-1/8 fixed top-0 left-0 m-4 font-mono sm:text-hidden lg:text-white lg:hover:text-blue-500 duration-150 hover:cursor-pointer hover:bg-neutral-900 flex flex-row justify-content-center text-2xl"}
+    <div className={"h-16 p-4 w-1/2 lg: w-1/8 fixed top-0 z-400 left-0 m-4 font-mono sm:text-hidden lg:text-white lg:hover:text-blue-500 duration-150 hover:cursor-pointer hover:bg-neutral-900 flex flex-row justify-content-center text-2xl"}
     onClick={() => {
         navigate('/')
     }}>
