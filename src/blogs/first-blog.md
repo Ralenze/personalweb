8fe0fc030ffb5e87c777ea261cac09a2b7b27d98
Vibhinn Gautam <bhagwan.vibhinn@hotmail.com>
2026-02-28 21:22:45 -0500

    update: modernized site a little bit

--- src/blogs/first-blog.md
+++ src/blogs/first-blog.md
@@ -1,24 +1,15 @@
 ---
-title: My First Post
-date: 2026-01-15
+title: Introduction
+date: 2026-02-28
 tags: React, SSMH
 excerpt: Learning how to yell into the void. Made with React.
 readingTime: 4 mins
 ---
 
-## Why I wrote this
+## Purpose
 
-I've been wanting to type some stuff here for some while. There's been so so so much to say
-The hardest part of building this website was having the motivation and discipline to build it.
+After debating for some time, I've decided to build up a blog that documents my journey as a software engineer and exciting events that shape my life. Occasionally, I'm thinking of posting opinions that I have on different topics.
 
-> Gimme dat World
+Basically, this is where I write down ideas and stuff that's worth sharing, but not through LinkedIn or IG or anything like that. It might also just be a monthly update on what I'm working on, fun places I've been to, and anything else. I don't think I'll be updating this frequently however.
 
-- Socrates
-
-## What I learned
-
-Ohh its such a bisssh
-
-## Final thoughts
-
-Done.
+Finally, I'll just highlight tech stacks that I work with: React, Python, TypeScript/JavaScript, HTML/CSS, Tailwind, Django, TensorFlow, PyTorch, C++, Kotlin, Playwright, Electron. I'm always willing to learn more, but this represents a little bit of what I can use today.
--- src/texts/aboutme.js
+++ src/texts/aboutme.js
@@ -4,18 +4,34 @@ export const ABOUTME = {
       { text: "Hey there! I am a 3rd year Computer Science student at the " },
       { text: "University of Waterloo", colour: "yellow" },
       {
-        text: ". One of my favourite activities is building software projects that solve real-world problems or just for fun. I have experience in full-stack development, and I enjoy working with technologies like React, Node.js, and Python.",
+        text: ". I love working on backend systems, developer tooling, and hardware-adjacent projects that solve meaningful real-world problems. Over the past few years, I’ve built a ground system to help pilots communicate with drones, AI tools that accelerate data annotation, a database pipeline for real-time gameplay storage, and a CNN model for autonomous landing-pad detection.",
+      },
+      { text: " ", style: "linebreak" },
+
+      {
+        text: "I’m constantly learning and growing as a backend engineer. These days, I'm also interested in computational programming and a drive to build reliable, scalable software. I love attending hackathons and tech conferences to understand and work with the latest technologies, and get free food on the side. My most recent hackathon was UOttaHack.",
+      },
+      { text: " ", style: "linebreak" },
+
+      {
+        text: "Outside of programming, I enjoy playing music, whether its on the piano, trumpet, or a euphonium. One of these days I'm looking to pick up the electric guitar and play a bunch of different songs such as Iris by Goo Goo Dolls. I also love listening to The Weeknd, Charli XCX, Drake, or anything thats EDM or Rock. Honestly, my playlists are a mix of everything, so I'm always happy to see new suggestions.",
       },
       { text: " ", style: "linebreak" },
       {
-        text: ` When I'm not typing away code on my keyboard, 
-        you can find me exploring different trails, 
-        working out and playing sports, or just travelling around. `,
+        text: " I'm also into working out and playing sports. I especially enjoy playing badminton, basketball, and ultimate. I'm really looking forward to getting my stamina up for long bike rides this summer, in spite all the injuries I've had from losing my balance.",
       },
       { text: " ", style: "linebreak" },
-      { text: ` I grew up in ` },
-      { text: `Vancouver, BC `, colour: "blue" },
-      { text: "where I attended elementary and high school." },
+
+      { text: "As a bit of a background, I grew up in " },
+      { text: "Vancouver, BC ", colour: "blue" },
+      {
+        text: "where I attended elementary and high school. I was involved in many clubs, such as our school's Math club, Business club, and Robotics club. This translated to me joining Waterloo Aerial Robotics Group, which is our university's drone team. Here, I worked to lead various ML-related projects, while also securing sponsorship funding to further autonomous coding capabilities.",
+      },
+      { text: " ", style: "linebreak" },
+
+      {
+        text: "If any of this resonates with you, feel free to reach out! I love meeting and working with new people to have fun and make great things possible. ",
+      },
     ],
   },
 };
